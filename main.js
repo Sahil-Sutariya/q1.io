15b8810a1185c358a72d8b8323b27c7a8aabb1a3
Sahil-Sutariya <55292204+Sahil-Sutariya@users.noreply.github.com>
2021-04-20 13:42:32 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+
+const image = document.getElementById("myImg");
+
+const myDiv = document.getElementById("myDiv");
+
+const allh1 = document.querySelector("h1");
+
+
+
+image.addEventListener("mouseover", e => {
+    image.src = './images/img2.jpg';
+    
+    var h1Tag = document.createElement("h1");      
+    h1Tag.innerHTML = "Good Luck On Your Final Exam!";                   
+    myDiv.appendChild(h1Tag);    
+
+
+});
+
+image.addEventListener("mouseout", e => {
+    image.src = './images/img1.jpg';
+    document.body.style.background = "#C0DE25";
+});
+
+
+
